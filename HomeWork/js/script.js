3adc16372ae6df9fc53bb5b2125d317b399f48e8
rsendziuk <sendzyuk.r@gmail.com>
2024-12-30 15:28:56 +0100

    hw

--- HomeWork/js/script.js
+++ HomeWork/js/script.js
@@ -9,8 +9,14 @@
 //   { id: 3, name: "Orange" },
 // ];
 
+// console.log(fruts.map((fruit) => fruit.name));
+
 //  -- 2 --
 //Виведіть парні числа від 2 до 10, використовуючи цикл for.
+
+// for (i = 2; i <= 10; i++) {
+//   if (i % 2 === 0) console.log(i);
+// }
 
 //  -- 3 --
 //Замініть цикл "for" на "while"
@@ -18,11 +24,25 @@
 //     console.lpg( `цифра ${i}!` );
 //   }
 
+// let i = 0;
+// while (i < 5) {
+//   console.log(`цифра ${i}!`);
+//   i++;
+// }
+
 //  -- 4 --
 //Напишіть цикл, який пропонує prompt ввести число більше за 100.
 //Якщо відвідувач введе менше число – попросити ввести ще раз, і так далі.
 //Цикл повинен запитувати число доти, доки відвідувач не введе число,
 // більше за 100, або не скасує ввід/введе порожній рядок.
+
+// let num = 0;
+// while (num < 100) {
+//   const input = +prompt(`Please provide a number (>100)`);
+//   console.log(input);
+//   if (!input) break;
+//   num = input;
+// }
 
 //  -- 5 --
 // Вирахуйте середній вік
@@ -33,3 +53,8 @@
 //   { age: 10, name: "Юля" },
 //   { age: 20, name: "Катя" },
 // ];
+
+// const averageAge =
+//   girls.reduce((acc, item) => acc + item.age, 0) / girls.length;
+
+// console.log(averageAge);
